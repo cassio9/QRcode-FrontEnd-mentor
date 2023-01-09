e647e78749015985aab4242fd3d93e9c8017fc04
Cassio Soares <cassiosoares9@hotmail.com>
2023-01-09 12:19:37 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 	return (
 		<div className="bg-[#D6E2F0] min-h-screen flex justify-center items-center w-[100%] text-base p-10 font-Outfit">
 			<div className="bg-white flex rounded-xl  flex-col p-4 max-w-[310px] ">
-				<img className="rounded-xl " src={QR} alt="QR code to frontendmentor.io" />
+				<img className="rounded-xl  " src={QR} alt="QR code to frontendmentor.io" />
 				<h1 className="text-center text-[#1F3251] p-4  font-[700] text-xl ">
 					Improve your front-end skills by building projects
 				</h1>
